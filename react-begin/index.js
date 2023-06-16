3fe7deb40024615a846a3ce858daa7c8aef60029
mirian228 <pariksini@mail.ru>
2023-06-16 15:39:27 +0400

    Seperating HTML tags to different components and rendering them

--- react-begin/index.js
+++ react-begin/index.js
@@ -1,11 +1,24 @@
-function MyPage() {
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./reactjs-icon.png" width="40px"></img>
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>@ 2023 Surmanidze development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function MainInfo() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./reactjs-icon.png" width="40px"></img>
-        </nav>
-      </header>
       <h1>Reasons To Learn React</h1>
       <p>JSX</p>
       <ul>
@@ -15,9 +28,16 @@ function MyPage() {
         <li>create full stack application</li>
         <li>good to go</li>
       </ul>
-      <footer>
-        <small>@ 2023 Surmanidze development. All rights reserved.</small>
-      </footer>
+    </div>
+  );
+}
+
+function MyPage() {
+  return (
+    <div>
+      <Header />
+      <MainInfo />
+      <Footer />
     </div>
   );
 }
