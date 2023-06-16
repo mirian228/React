996aa6159b536f1271f30f00a630e6b5cd3e44c0
mirian228 <pariksini@mail.ru>
2023-06-16 14:21:04 +0400

    react basics

---
+++ react-begin/index.js
@@ -0,0 +1,15 @@
+const jsxPage = (
+  <div>
+    <img src="./reactjs-icon.png" width="40px"></img><a>ReactFacts</a>
+    <h1>Fun facts about ReactDOM</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(jsxPage, document.querySelector("#root"));
